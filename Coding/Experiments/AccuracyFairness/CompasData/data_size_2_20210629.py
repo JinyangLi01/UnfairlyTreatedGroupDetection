b6adb01aa06e91f5f093d74563fd8e1586c7937f
JinyangLi01 <jinyli@umich.edu>
2021-06-29 17:51:02 +0800

    finish exp for low acc

--- Coding/Experiments/AccuracyFairness/CompasData/data_size_2_20210629.py
+++ Coding/Experiments/AccuracyFairness/CompasData/data_size_2_20210629.py
@@ -1,9 +1,9 @@
 """
 This script is to do experiment on the data size.
 
-RecidivismData_att_classified.txt: 6889 rows
+CompasData_att_classified.txt: 6889 rows
 data sizes: 100, 500, 1000, 2000, 3000, 4000, 5000, 6000
-selected randomly, and generate files in InputData/RecidivismData/DifferentDataSizes/
+selected randomly, and generate files in InputData/CompasData/DifferentDataSizes/
 
 
 
@@ -28,7 +28,6 @@ from Algorithms import NaiveAlg_1_20210528 as naivealg
 from Algorithms import Predict_0_20210127 as predict
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
-
 SMALL_SIZE = 8
 MEDIUM_SIZE = 10
 BIGGER_SIZE = 20
@@ -74,7 +73,6 @@ def GridSearch(original_data_file_pathpre, datasize, thc, selected_attributes):
     overall_acc = 1 - len(mis_class_data) / len(less_attribute_data)
 
     tha = overall_acc - 0.2
-
     print("tha = {}, thc = {}".format(tha, thc))
     pattern_with_low_accuracy1, num_calculation1, execution_time1 = newalg.GraphTraverse(less_attribute_data,
                                                                                          mis_class_data, tha,
@@ -95,21 +93,23 @@ def GridSearch(original_data_file_pathpre, datasize, thc, selected_attributes):
 
 
     if execution_time1 > time_limit:
-        print("optimized alg exceeds time limit")
+        print("new alg exceeds time limit")
     if execution_time2 > time_limit:
         print("naive alg exceeds time limit")
 
 
-    return execution_time1, num_calculation1, execution_time2, num_calculation2, pattern_with_low_accuracy1
+    return execution_time1, num_calculation1, execution_time2, num_calculation2, pattern_with_low_accuracy1, overall_acc, tha
 
 
-selected_attributes = ['limit_bal', 'sex', 'education', 'marriage', 'age', 'pay_0', 'pay_2']
-data_sizes = [30000, 35000, 40000, 45000, 50000, 55000, 60000]
+selected_attributes = ['sexC', 'ageC', 'raceC', 'MC', 'priors_count_C', 'c_charge_degree',
+                       'decile_score']
+data_sizes = [6000, 8000, 10000, 12000, 14000, 16000, 18000, 20000]
 Thc = 50
-original_data_file_pathprefix = "../../../../InputData/CreditcardDataset/LargeDatasets_cat/"
+original_data_file_pathprefix = "../../../../InputData/CompasData/LargerDatasets/"
 
 
-time_limit = 10*60
+
+time_limit = 20*60
 # based on experiments with the above parameters, when number of attributes = 8, naive algorithm running time > 10min
 # so for naive alg, we only do when number of attributes <= 7
 execution_time1 = list()
@@ -118,15 +118,18 @@ num_patterns_checked1 = list()
 num_patterns_checked2 = list()
 num_patterns_found = list()
 patterns_found = list()
+overall_acc_list = list()
+tha_list = list()
 num_loops = 1
-
+overall_acc = 0
+tha = 0
 
 for datasize in data_sizes:
     print('\n\ndatasize = {}'.format(datasize))
     t1, t2, calculation1, calculation2 = 0, 0, 0, 0
     result_cardinality = 0
     for l in range(num_loops):
-        t1_, calculation1_, t2_, calculation2_, result = \
+        t1_, calculation1_, t2_, calculation2_, result, overall_acc, tha  = \
             GridSearch(original_data_file_pathprefix, datasize, Thc, selected_attributes)
         t1 += t1_
         t2 += t2_
@@ -145,11 +148,12 @@ for datasize in data_sizes:
     num_patterns_checked1.append(calculation1)
     execution_time2.append(t2)
     num_patterns_checked2.append(calculation2)
+    overall_acc_list.append(overall_acc)
+    tha_list.append(tha)
 
 
 
-
-output_path = r'../../../../OutputData/LowAccDetection/CreditcardDataset/data_size.txt'
+output_path = r'../../../../OutputData/LowAccDetection/CompasDataset/data_size.txt'
 output_file = open(output_path, "w")
 num_lines = len(execution_time1)
 
@@ -158,9 +162,15 @@ for n in range(len(data_sizes)):
     output_file.write('{} {} {}\n'.format(data_sizes[n], execution_time1[n], execution_time2[n]))
 
 
-output_file.write("\n\nnumber of patterns visited\n")
+output_file.write("\n\nnumber of calculations\n")
 for n in range(len(data_sizes)):
     output_file.write('{} {} {}\n'.format(data_sizes[n], num_patterns_checked1[n], num_patterns_checked2[n]))
+
+
+output_file.write("\n\nnumber of patterns found\n")
+for n in range(len(data_sizes)):
+    output_file.write('{} {} \n {}\n'.format(data_sizes[n], num_patterns_found[n], patterns_found[n]))
+
 
 
 fig, ax = plt.subplots()
@@ -168,11 +178,11 @@ plt.plot(data_sizes, execution_time1, label="optimized algorithm", color='blue',
 plt.plot(data_sizes, execution_time2, label="naive algorithm", color='orange', linewidth = 3.4)
 plt.xlabel('data size (K)')
 plt.ylabel('execution time (s)')
-plt.xticks([30000, 40000, 50000, 60000])
+plt.xticks([6000, 8000, 10000, 12000, 14000, 16000, 18000, 20000])
 ax.xaxis.set_major_formatter(FuncFormatter(thousands_formatter))
 plt.subplots_adjust(bottom=0.15, left=0.18)
 plt.legend()
-plt.savefig("../../../../OutputData/LowAccDetection/CreditcardDataset/datasize_time.png")
+plt.savefig("../../../../OutputData/LowAccDetection/CompasDataset/datasize_time.png")
 plt.show()
 
 
@@ -180,13 +190,13 @@ fig, ax = plt.subplots()
 plt.plot(data_sizes, num_patterns_checked1, label="optimized algorithm", color='blue', linewidth=3.4)
 plt.plot(data_sizes, num_patterns_checked2, label="naive algorithm", color='orange', linewidth=3.4)
 plt.xlabel('data size (K)')
-plt.xticks([30000, 40000, 50000, 60000])
+plt.xticks([6000, 8000, 10000, 12000, 14000, 16000, 18000, 20000])
 ax.xaxis.set_major_formatter(FuncFormatter(thousands_formatter))
 plt.ylabel('number of nodes visited (K)')
 ax.yaxis.set_major_formatter(FuncFormatter(thousands_formatter))
 plt.subplots_adjust(bottom=0.15, left=0.18)
 plt.legend()
-plt.savefig("../../../../OutputData/LowAccDetection/CreditcardDataset/datasize_calculations.png")
+plt.savefig("../../../../OutputData/LowAccDetection/CompasDataset/datasize_calculations.png")
 plt.show()
 
 
