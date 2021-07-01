bad0c0e587ed5c516b3a9f149dc1c2308761f311
JinyangLi01 <jinyli@umich.edu>
2021-07-01 11:28:10 +0800

    ranking, compas, num att, general, adult, numatt

--- Coding/Experiments/General_FP/AdultData/num_attributes_1_20210629.py
+++ Coding/Experiments/General_FP/AdultData/num_attributes_1_20210629.py
@@ -8,7 +8,7 @@ x axis: the number of attributes, from 2 to 13.
 
 other parameters:
 CleanAdult2.csv
-threshold of cardinality Thc = 30
+size threshold Thc = 30
 threshold of minority group accuracy: overall acc - 20
 """
 
@@ -224,7 +224,7 @@ for n in range(num_att_max_naive, num_att_max):
 
 output_file.write("\n\nnumber of patterns found\n")
 for n in range(num_att_min, num_att_max):
-    output_file.write('{} {} \n {}\n'.format(n, num_patterns_found[n], patterns_found[n]))
+    output_file.write('{} {} \n {}\n'.format(n, num_patterns_found[n-num_att_min], patterns_found[n-num_att_min]))
 
 
 
@@ -251,7 +251,7 @@ fig, ax = plt.subplots()
 plt.plot(x_new, num_calculation1, label="optimized algorithm", color='blue', linewidth = 3.4)
 plt.plot(x_naive, num_calculation2, label="naive algorithm", color='orange', linewidth = 3.4)
 plt.xlabel('number of attributes')
-plt.ylabel('number of nodes visited (K)')
+plt.ylabel('number of patterns visited (K)')
 ax.yaxis.set_major_formatter(FuncFormatter(thousands_formatter))
 
 
